d4f3bba00d20d8b9cadf132e518db677ef4d5876
hanisharun11 <muhdhanis08@gmail.com>
2020-12-21 10:46:25 +0800

    add ux for show single trainer

--- resources/views/trainings/show.blade.php
+++ resources/views/trainings/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Show Training') }}</div>
+                <div class="card-header">{{ __('Show Training') }} by {{$training->user->name}}</div>
 
                 <div class="card-body">
                 <form method="post" action="">
