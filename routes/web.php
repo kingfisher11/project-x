09ebe1b02dd82f9324e6e5318a2e6b3fcc9b1f38
Muhd Hanis Harun <muhdhanis08@gmail.com>
2020-12-30 23:04:56 +0800

    add localization

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,5 @@ Route::post('/trainings/{id}/edit',[App\Http\Controllers\TrainingController::cla
 Route::get('/trainings/{training}/delete',[App\Http\Controllers\TrainingController::class, 'delete'])->name('training:delete');
 Route::get('/trainings/{training}/forceDelete',[App\Http\Controllers\TrainingController::class, 'forceDelete'])->name('training:forceDelete');
 Route::get('/admin/audits', [App\Http\Controllers\AuditController::class, 'audit'])->middleware(['auth','admin'])->name('training:audit');
+
+Route::get('/language/{locale}', [App\Http\Controllers\LocalizationController::class,'changelocale']);
